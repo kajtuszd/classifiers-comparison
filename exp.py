be627540972f7acc5f5d96234ddf9ba58a0b56a7
kajetan <kajetanzdanowicz@wp.pl>
2023-01-19 02:34:36 +0100

    saving pair tests results

--- exp.py
+++ exp.py
@@ -11,60 +11,42 @@ from tabulate import tabulate
 
 
 files = [
-    # 'aids.csv',
-    # 'amazon_employee_access.csv',
-    # 'balance-scale.csv',
-    # 'chorobySercaAfrica.csv',
-    # 'diabetes.csv',
-    # 'digits.csv',
-    # 'ecoli.csv',
-    # 'fruitfly.csv',
-    # 'id12-Statlog_australia.csv',
-    # 'id14-brest-cancer.csv',
-    # 'id18-o-ring-erosion-or-blowby.csv',
-    # 'id4-cars.csv',
-    # 'id6-Ionosphere.csv',
-    # 'id8-congress-vote-record.csv',
-    # 'monks.csv',
-    # 'pendigits.csv',
-    # 'phishing.csv',
-    # 'plamyOleju.csv',
-    # 'shuttle.csv'
-    # 'stock.csv',
-    # 'texturaPatch.csv',
-    # 'ttt.csv',
-    # 'vehicles.csv',
-
-
-    ## NOWE PONIZEJ Z KEELA
-    # 'vowel.csv',
-    # 'texture.csv',
+    'balance-scale.csv',
     # 'movement_libras.csv',
-    # 'letter.csv',
-    # 'led7digit.csv',
-    # 'page-blocks.csv',
-    # 'satimage.csv',
-    # 'segment.csv',
-    # 'winequality-red.csv',
-    # 'winequality-white.csv',
+    # 'texturaPatch.csv',
     # 'contraceptive.csv',
     # 'newthyroid.csv',
+    # 'texture.csv',
+    # 'page-blocks.csv',
+    'vehicles.csv',
+    # 'digits.csv',
+    # 'pendigits.csv',
+    # 'vowel.csv', # chyba nie dziala
+    # 'id18-o-ring-erosion-or-blowby.csv', chyba tez
+    # 'satimage.csv',
+    # 'winequality-red.csv',
+    # 'id4-cars.csv',
+    # 'segment.csv',
+    # 'winequality-white.csv',
+    # 'led7digit.csv',
+    # 'shuttle.csv',
+    # 'letter.csv',
     # 'tae.csv',
-
-    'yeast.csv', # nie dziala dla gaussa z jakiegos powodu z baggingiem
-    # 'abalone.csv' # ta sama sytuacja
+    'zoo.csv',
 ]
 
 
-def ensemble_tests(files, num_of_estimators, EnsembleClass, classifiers):
+def ensemble_tests(files, num_of_estimators, EnsembleClass, classifiers, result_filename):
     alpha = .05
     n_folds = 5
     t_statistic = np.zeros((len(classifiers), len(classifiers)))
     p_value = np.zeros((len(classifiers), len(classifiers)))
-    kf = KFold(n_splits=n_folds, shuffle=True, random_state=1234)
+    means = np.zeros((len(files), len(classifiers)))
+    pair_tests_sum = np.zeros((len(classifiers), len(classifiers)))
 
-    for file in files:
-        dataset = np.genfromtxt(f"new_datasets/{file}", delimiter=",")
+    kf = KFold(n_splits=n_folds, shuffle=True, random_state=1234)
+    for file_ind, file in enumerate(files):
+        dataset = np.genfromtxt(f"datasets_multi/{file}", delimiter=",")
         X = dataset[:, :-1]
         y = dataset[:, -1].astype(int)
         scores = np.zeros((len(classifiers), n_folds))
@@ -85,8 +67,10 @@ def ensemble_tests(files, num_of_estimators, EnsembleClass, classifiers):
         # mean classifier scores
         print(f"\nDataset file: {file}")
         print(scores)
+
         mean = np.mean(scores, axis=1)
         std = np.std(scores, axis=1)
+        means[file_ind] = mean
 
         for clf_id, (clf_name, clf) in enumerate(classifiers.items()):
             print("%s: %.3f (%.2f)" % (clf_name, mean[clf_id], std[clf_id]))
@@ -96,43 +80,41 @@ def ensemble_tests(files, num_of_estimators, EnsembleClass, classifiers):
             for j in range(len(classifiers)):
                 t_statistic[i, j], p_value[i, j] = ttest_rel(scores[i],
                                                              scores[j])
-        # print("t-statistic:\n", t_statistic, "\n\np-value:\n", p_value)
         headers = [key for key in classifiers.keys()]
         names_column = np.array([[key] for key in classifiers.keys()])
-        t_statistic_table = np.concatenate((names_column, t_statistic), axis=1)
-        # t_statistic_table = tabulate(t_statistic_table, headers, floatfmt=".2f")
-        # p_value_table = np.concatenate((names_column, p_value), axis=1)
-        # p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
-        # print("t-statistic:\n", t_statistic_table, "\n\np-value:\n", p_value_table)
         advantage = np.zeros((len(classifiers), len(classifiers)))
         advantage[t_statistic > 0] = 1
-        # advantage_table = tabulate(np.concatenate(
-        #     (names_column, advantage), axis=1), headers)
-        # print("Advantage:\n", advantage_table)
         significance = np.zeros((len(classifiers), len(classifiers)))
         significance[p_value <= alpha] = 1
-        # significance_table = tabulate(np.concatenate(
-        #     (names_column, significance), axis=1), headers)
-        # print("Statistical significance (alpha = 0.05):\n", significance_table)
         stat_better = significance * advantage
         stat_better_table = tabulate(np.concatenate(
             (names_column, stat_better), axis=1), headers)
+        pair_tests_sum += stat_better
         print("Statistically significantly better:\n", stat_better_table)
+    np.save(f'./results/{result_filename}', means)
+    np.save(f'./results_pair_stats/{result_filename}', pair_tests_sum)
 
+
+filenames_b = {'bagging_5': 5, 'bagging_10': 10, 'bagging_15': 15}
+filenames_a = {'adaboost_5': 5, 'adaboost_10': 10, 'adaboost_15': 15}
 
 bagging_clfs = {
     'GNB': GaussianNB(),
-    'CART': DecisionTreeClassifier(),
+    'CART': DecisionTreeClassifier(random_state=1234),
     'MLP': MLPClassifier(),
 }
 print(f"**********\nBAGGING\n**********")
-ensemble_tests(files=files, num_of_estimators=2, EnsembleClass=BaggingClassifier, classifiers=bagging_clfs)
+
+for file, num in filenames_b.items():
+    ensemble_tests(files=files, num_of_estimators=num, EnsembleClass=BaggingClassifier, classifiers=bagging_clfs, result_filename=file)
 
 
 adaboost_clfs = {
     'GNB': GaussianNB(),
-    'CART': DecisionTreeClassifier(),
+    'CART': DecisionTreeClassifier(random_state=1234),
 }
 print(f"**********\nADABOOST\n**********")
-ensemble_tests(files=files, num_of_estimators=2, EnsembleClass=AdaBoostClassifier, classifiers=adaboost_clfs)
+
+for file, num in filenames_a.items():
+    ensemble_tests(files=files, num_of_estimators=num, EnsembleClass=AdaBoostClassifier, classifiers=adaboost_clfs, result_filename=file)
 
